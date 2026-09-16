4bd0f11f41c2ea333e2229b6d3f248f831df3042
sevenam <johnbt@outlook.com>
2026-09-17 00:55:10 +0200

    bump version to 0.11.0

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	appName = "Gitraffe"
-	version = "0.10.1"
+	version = "0.11.0"
 
 // logFileName is initialized at runtime in main so we can compute
 // a platform-appropriate location (cache/log dir) instead of using the
